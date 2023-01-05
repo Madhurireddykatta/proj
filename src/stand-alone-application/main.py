9b7e18a1f4ecfcf5661dd1ed3bccd42788e6a17e
Madhurireddykatta <77008825+Madhurireddykatta@users.noreply.github.com>
2023-01-05 15:55:47 +0530

    Update main.py

--- src/stand-alone-application/main.py
+++ src/stand-alone-application/main.py
@@ -99,7 +99,7 @@ def main():
 	# Attributes of main dialog box
 	global form
 	form = Tkinter.Tk()
-	export DISPLAY=0.0
+	export DISPLAY=:0.0
 	form.wm_title('thrain')
 	'''
 	-------------------------------------------------------------
